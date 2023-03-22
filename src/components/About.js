6e18d13a4c0263f009a27d277036945dd6d03ac2
jeremygradisher <jeremygradisher@gmail.com>
2023-03-22 13:32:06 -0400

    Adjusted text on about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ export default function About() {
   return (
     <main>
       <p>
-        This is a Single-page application (SPA) built with React with dynamic, client-side routing. In a sense it does appear to have different pages. The magic of it is that the pages are components are they are displayed without a page request!!!
+        This is a Single-page application (SPA) built with React that has dynamic, client-side routing. In a sense it does appear to have different pages. The magic of it is that the pages are components and they are displayed without a http request which makes it lightning fast!!!
+      </p>
+      <p>
+        Feel free to click around. This tutorial showed building the navigation and creating the routing with react-router-dom. There is dynamic page creation with authors and articles that have categories.
       </p>
     </main>
   );
