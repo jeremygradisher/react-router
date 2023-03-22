44a651313ea9a5551070abbd5e36cc1ff67a0086
jeremygradisher <jeremygradisher@gmail.com>
2023-03-22 13:24:42 -0400

    Altered text on about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <main>
       <p>
-        This application contains snippets from various Codecademy articles. You can browse all our articles and read them in their entirety....
+        This is a Single-page React.js application with with dynamic, client-side routing. In a sense it does appear to have different pages. The magic of it is that the pages are components are they are displayed without a page request!!!
       </p>
     </main>
   );
